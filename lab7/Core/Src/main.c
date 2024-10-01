ad98a9a6c7c8881f018131f4c7c1d552880cd462
RolindaB <asrolin15@gmail.com>
2024-09-30 22:36:54 -0600

    xx

--- lab7/Core/Src/main.c
+++ lab7/Core/Src/main.c
@@ -1,17 +1,11 @@
 /* USER CODE BEGIN Header */
 /**
   ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
+  * Rolinda Beb          : 22274
+  * LAb7                 : Digital 2
   ******************************************************************************
-  * @attention
+  * ALmacenamiento en SD
   *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
   *
   ******************************************************************************
   */
